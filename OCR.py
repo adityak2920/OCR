c2f97020971035613175b204e9ef028520fda877
Aditya Kumar <35501699+adityak2920@users.noreply.github.com>
2019-04-15 13:14:34 +0530

    Update OCR.py

--- OCR.py
+++ OCR.py
@@ -4,21 +4,25 @@ import pytesseract
 import urllib.request
 import numpy as np
 
+# IP address from ipwebcam app
 url = "http://192.168.43.1:8080/shot.jpg"
 
 while True:
+    # Extracting from url and converting it to image format so that opencv can read
     img_response = urllib.request.urlopen(url)
     img_numpy = np.array(bytearray(img_response.read()), dtype=np.uint8)
     frame = cv2.imdecode(img_numpy, -1)
     config = ('-l eng --oem 1 --psm 3')
 
-    # Read image from disk
+    # Read image for OCR
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Run tesseract OCR on image
     text = pytesseract.image_to_string(gray, config=config)
 
-#         Print recognized text
+    # Print recognized text
     print(text)
+    
+    # Displaying the video
     cv2.imshow('IPWebcam', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
